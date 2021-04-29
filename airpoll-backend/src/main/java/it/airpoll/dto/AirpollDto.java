86ec74e7958decd08b15b1cd4af482c46fd683ad
c-giacomo <c.giacomo@hotmail.it>
2021-04-29 12:45:48 +0200

    adding projection

--- airpoll-backend/src/main/java/it/airpoll/dto/AirpollDto.java
+++ airpoll-backend/src/main/java/it/airpoll/dto/AirpollDto.java
@@ -1,6 +1,8 @@
 package it.airpoll.dto;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -33,5 +35,28 @@ public class AirpollDto {
 	private Double longitude;
 	
 	private LocalDate local;
+	
+	public AirpollDto() {}
+	
+	public AirpollDto(String location, String city, String country, Double pm25, Double no2, Double co, Double pm10, Double o3, 
+			Double latitude, Double longitude, Date local) {
+		this.location = location;
+		this.city = city;
+		this.country = country;
+		this.pm25 = pm25;
+		this.no2 = no2;
+		this.co = co;
+		this.pm10 = pm10;
+		this.o3 = o3;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.local = convertToLocalDateViaInstant(local);
+	}
+	
+	private LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+	    return dateToConvert.toInstant()
+	      .atZone(ZoneId.systemDefault())
+	      .toLocalDate();
+	}
 	
 }
--- airpoll-backend/src/main/java/it/airpoll/repository/CountryRepository.java
+++ airpoll-backend/src/main/java/it/airpoll/repository/CountryRepository.java
@@ -1,9 +1,14 @@
 package it.airpoll.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import it.airpoll.dto.AirpollDto;
 import it.airpoll.model.Country;
 
 /**
@@ -15,5 +20,16 @@ import it.airpoll.model.Country;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Integer>, JpaSpecificationExecutor<Country> {
+	
+	@Query("select new it.airpoll.dto.AirpollDto(l.identifier, c.name, co.name, m.pm25, m.no2, m.co, m.pm10, m.o3, l.latitude, l.longitude, m.local)"
+			+ "	from"
+			+ "	City c, Country co, Location l, Measurements m"
+			+ "	where"
+			+ "	co.id = c.country.id AND"
+			+ "	c.id = l.city.id AND"
+			+ "	l.id = m.location.id AND"
+			+ " (:countryID is null OR co.id = :countryID) AND"
+			+ " (:cityID is null OR c.id = :cityID)")
+	public List<AirpollDto> readAll(@Param("countryID") Integer countryID, @Param("cityID") Integer cityID);
 
 }
